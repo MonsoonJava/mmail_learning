42cabc76a9c0c83a4d99e77a1449835bd551dd1d
monsoon <357703850@qq.com>
2017-07-02 17:42:54 +0800

    commit about spring context and mybatis plus

---
+++ src/main/java/com/xfj/mmail/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.xfj.mmail.dao;
+
+import com.xfj.mmail.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/xfj/mmail/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.xfj.mmail.dao;
+
+import com.xfj.mmail.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
